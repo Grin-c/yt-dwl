e943cc6a31de9db7617d566e61cf1e0b2aef6721
rodf <rodf@pop-os.localdomain>
2021-12-17 20:21:27 -0300

    😀

--- src/main/Download.py
+++ src/main/Download.py
@@ -6,8 +6,7 @@ from pytube import YouTube
 from pytube import exceptions
 
 def FilePath():
-    global file_path
-    file_path = str(input("\nDiretório de Download: "))
+    file_path = str(input("\nDiretório Para o Download: "))
     home = os.getenv("HOME") + "/"
     dir_default_download = home + "/Downloads/yt-dwl"
     directorys = file_path.split("/")
@@ -23,10 +22,10 @@ def FilePath():
 
         file_path = dir_default_download
         print(f"\n\033[1;33m[WARNING] \033[1;37mDiretório de Download Definido Para: {file_path} ")
-
+    
     if os.path.isdir(file_path) == False:
-        print("\nDiretório inexistente\n")
-        FilePath()  
+        print("\n\033[1;31m[ERROR]\033 Diretório Inexistente\n")
+        exit()
 
     return file_path 
 
@@ -38,10 +37,10 @@ def SetVideo():
         video = YouTube(url, on_progress_callback=BarDownloadProgress)
     except exceptions.RegexMatchError:
         print("\n\033[1;31m[ERROR]\033[1;37m link inválido")
-        SetVideo()
+        exit()
     except exceptions.VideoUnavailable: 
         print("\n\033[1;31m[ERROR]\033[1;37m Video indisponível") 
-        SetVideo()
+        exit()
 
     titulo = video.title 
     lTitulo = []
@@ -64,9 +63,6 @@ def BarDownloadProgress(chunk, file_handle, bytes_remaining):
 def DownloadVideo(dir_path, titulo):
     file_path = f"{dir_path}/{titulo+'.mp4'}"
 
-    global stream
-    stream = video.streams.get_highest_resolution()
-
     while os.path.exists(file_path):
         print("\n\033[1;31m[ERROR]\033[1;37m Nome de arquivo já existente nesse diretório\n")
         titulo = str(input("Nome do arquivo: "))
@@ -75,26 +71,28 @@ def DownloadVideo(dir_path, titulo):
     print("\n\033[1;33m[WARNING] \033[1;37mBaixando o vídeo na melhor resolução\n")
 
     print(f"Downloading: {titulo}")
-    stream.download(dir_path, titulo+'.mp4')
+    global stream
+    stream = video.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").first()
+    stream.download(dir_path)
     print(f"\n\n\033[1;32mDownload Concluido => {file_path}\033[1;37m\n")
 
 def DownloadAudio(dir_path, titulo):
     file_path = f"{dir_path}/{titulo+'.mp3'}"
-    file_path_upper = f"{dir_path}/{titulo.upper()+'.mp3'}"
-
-    global stream
-    stream = video.streams.get_audio_only()
+    file_path_orig = f"{dir_path}/{titulo}"
 
     while os.path.exists(file_path):
-        print("\n\033[1;31m[ERROR]\033[1;37m Nome de arquivo já existente nesse diretório\n")
+        print("\n\033[1;31m[ERROR]\033[1;37m Nome de arquivo já existente nesse diretório, defina outro nome\n")
         titulo = str(input("Nome do arquivo: "))
         file_path = f"{dir_path}/{titulo+'.mp3'}"
+        file_path_orig = f"{dir_path}/{titulo}"
 
     print(f"Downloading: {titulo}")
-    stream.download(dir_path, titulo.upper()+".mp3")
+    global stream
+    stream = video.streams.filter(only_audio=True).first()
+    stream.download(dir_path, titulo)
     print(f"\n\n\033[1;32mDownload Concluido => {file_path}\033[1;37m\n")
 
     print(f"\nConvertendo: {titulo}")
-    os.system(f"ffmpeg -i '{file_path_upper}' -vn -ar 44100 -ac 2 -b:a 192k '{file_path}'")
-    os.system(f"rm '{file_path_upper}'")
+    os.system(f"ffmpeg -i '{file_path_orig}' -vn -ar 44100 -ac 2 -b:a 192k '{file_path}'")
+    os.system(f"rm '{file_path_orig}'")
     print(f"\n\033[1;32mConversão Concluida => {file_path}\033[1;37m\n")
