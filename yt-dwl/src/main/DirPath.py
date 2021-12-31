edd28d78ade26b447fa1ff16b7c9ee9e6e68735c
Rofen <rofen1@protonmail.com>
2021-12-30 21:43:21 -0300

    😁

---
+++ yt-dwl/src/main/DirPath.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import os
+
+class DirPath:
+    def __init__(self):
+        self.dir_path = str(input("\nDiretório Para o Download: "))
+        self.home = os.getenv("HOME") + "/"
+        self.dir_default_download = self.home + "/Downloads/yt-dwl"
+        self.directorys = self.dir_path.split("/")
+
+    def SetDir(self):
+        # Definindo o '~' como diretório raiz caso fosse pasado
+        if "~" in self.directorys:
+            self.directorys = self.directorys[1:len(self.directorys)]
+            self.strDire = "/".join(self.directorys)
+            self.dir_path = self.home + self.strDire
+
+        # Definindo um diretório padrão caso não fosse passado nenhum
+        if len(self.dir_path) == 0:    
+            # Criando diretório caso não exista
+            if not os.path.exists(self.dir_default_download):
+                os.makedirs(self.dir_default_download)
+
+            self.dir_path = self.dir_default_download
+            print(f"\n\033[1;33m[WARNING] \033[1;37mDiretório de Download Definido Para: {self.dir_path} ")
+        
+        if os.path.isdir(self.dir_path) == False:
+            print("\n\033[1;31m[ERROR]\033 Diretório Inexistente\n")
+            exit()
+
+        return self.dir_path
---
+++ yt-dwl/src/main/Download.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+from pytube import YouTube
+from pytube import exceptions
+
+class Download:
+    def __init__(self, url, dir_path):
+        self.url = url
+        self.dir_path = dir_path
+
+    def BarDownloadProgress(self, chunk, file_handle, bytes_remaining):
+        filesize = self.stream.filesize
+        current = ((filesize - bytes_remaining) / filesize)
+        percent = ('{0:.1f}').format(current * 100)
+        progress = int(50 * current)
+        status = '█' * progress + '-' * (50 - progress)
+        sys.stdout.write('↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
+        sys.stdout.flush()
+
+    def SetVideo(self):
+        try:
+            self.video = YouTube(self.url, on_progress_callback=self.BarDownloadProgress)
+        except exceptions.RegexMatchError:
+            print("\n\033[1;31m[ERROR]\033[1;37m link inválido")
+            exit()
+        except exceptions.VideoUnavailable:
+            print("\n\033[1;31m[ERROR]\033[1;37m Video indisponível")
+            exit()
+
+        # Formatando o título
+        self.titulo = self.video.title
+        self.titulo_split = self.titulo.split()
+        self.lTitulo = []
+
+        for c in range(len(self.titulo_split)):
+            self.letras = list(self.titulo_split[c])
+            for d in range(len(self.letras)):
+                if self.letras[d] == "/" or self.letras[d] == "'/'" or self.letras[d] == '"' or self.letras[d] == "'":
+                    pass
+                else:
+                    self.lTitulo.append(self.letras[d])
+            self.lTitulo.append(" ")
+
+        self.titulo = "".join(self.lTitulo)
+        self.titulo = self.titulo.rstrip()
+
+    def DownloadVideo(self):
+        self.file_path = f"{self.dir_path}/{self.titulo + '.mp4'}"
+        # Verificando se o arquivo existe
+        if os.path.exists(self.file_path):
+            opt = str(input("\n\033[1;33m[WARNING]\033[1;37m Arquivo já existente nesse diretório, Substituir? [S/N] ")).upper()
+            if opt == "N":
+                print("\n\033[1;33m[WARNING]\033[1;37m Download cancelado\n")
+                return
+            elif opt == "S":
+                os.system(f"rm '{self.file_path}'")
+            else:
+                print("\n\033[1;31m[ERROR] Opção inválida\033[1;37m")
+                return
+
+        print("\n\033[1;33m[WARNING] \033[1;37mBaixando o vídeo na melhor resolução\n")
+
+        print(f"Downloading: {self.titulo}")
+        self.stream = self.video.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").first()
+        self.stream.download(self.dir_path)
+        print(f"\n\n\033[1;32mDownload Concluido => {self.file_path}\033[1;37m\n")
+
+    # Baixando o audio
+    def DownloadAudio(self):
+        self.file_path = f"{self.dir_path}/{self.titulo + '.mp3'}"
+        self.file_path_orig = f"{self.dir_path}/{self.titulo}"
+
+        # Verificando se o arquivo existe
+        if os.path.exists(self.file_path):
+            opt = str(input("\n\033[1;33m[WARNING]\033[1;37m Arquivo já existente nesse diretório, Substituir? [S/N] ")).upper()
+            if opt == "N":
+                print("\n\033[1;33m[WARNING]\033[1;37m Download cancelado\n")
+                return
+            elif opt == "S":
+                os.system(f"rm '{self.file_path}'")
+            else:
+                print("\n\033[1;31m[ERROR] Opção inválida\033[1;37m")
+                return
+
+        print(f"Downloading: {self.titulo}")
+        self.stream = self.video.streams.filter(only_audio=True).first()
+        self.stream.download(self.dir_path, self.titulo)
+        print(f"\n\n\033[1;32mDownload Concluido => {self.file_path_orig}\033[1;37m\n")
+
+        print(f"\nConvertendo: {self.titulo}")
+        os.system(f"ffmpeg -i '{self.file_path_orig}' -vn -ar 44100 -ac 2 -b:a 192k '{self.file_path}'")
+        os.system(f"rm '{self.file_path_orig}'")
+        print(f"\n\033[1;32mConversão Concluida => {self.file_path}\033[1;37m\n")
