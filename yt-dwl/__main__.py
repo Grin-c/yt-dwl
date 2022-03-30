e92081bbe70daff9343def934c54b5122af30927
Grin-c <Grin-c@blala>
2022-03-30 18:53:41 -0300

    Remove print of the error message KeyboardInterrupt

--- yt-dwl/__main__.py
+++ yt-dwl/__main__.py
@@ -5,35 +5,38 @@ from time import sleep
 from src.main.Download import Download
 from src.main.DirPath import DirPath
 
-# Animação de Loading
-for c in range(1,4):
-        ponto = "." * c
-        sys.stdout.write(f"\033[1;37mLoading{ponto}\r")
-        sys.stdout.flush()
-        sleep(0.25)
+try: 
+    # Animação de Loading
+    for c in range(1,4):
+            ponto = "." * c
+            sys.stdout.write(f"\033[1;37mLoading{ponto}\r")
+            sys.stdout.flush()
+            sleep(0.25)
 
-print('''Download Options
+    print('''Download Options
 
-[0] => Video
-[1] => Áudio
-''')
-opt = int(input("Download Option: "))
+    [0] => Video
+    [1] => Áudio
+    ''')
+    opt = int(input("Download Option: "))
 
-# Verificando se a opção é valida
-if opt < 0 or opt > 1:
-    print("\n\033[1;31m[ERROR]\033[1;37m Número inválido\n")
-    exit()
+    # Verificando se a opção é valida
+    if opt < 0 or opt > 1:
+        print("\n\033[1;31m[ERROR]\033[1;37m Número inválido\n")
+        exit()
 
-# Definindo diretório
-dir_path = DirPath().SetDir()
+    # Definindo diretório
+    dir_path = DirPath().SetDir()
 
-# Baixando o formato do vídeo de acordo com a opção
-while True:
-    url = str(input("URL do vídeo: "))
-    download = Download(url, dir_path)
-    download.SetVideo()
+    # Baixando o formato do vídeo de acordo com a opção
+    while True:
+        url = str(input("URL do vídeo: "))
+        download = Download(url, dir_path)
+        download.SetVideo()
 
-    if opt == 0:
-        download.DownloadVideo()
-    else:
-        download.DownloadAudio()
+        if opt == 0:
+            download.DownloadVideo()
+        else:
+            download.DownloadAudio()
+except KeyboardInterrupt:
+    exit(print())
